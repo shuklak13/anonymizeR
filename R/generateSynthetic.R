e732f045ca614246aa4620219512fff68a078a84
Karan <shuklak13@gmail.com>
2016-01-14 10:00:34 -0600

    generateSynthetic: Documentation + Var Name Changes

--- R/generateSynthetic.R
+++ R/generateSynthetic.R
@@ -1,24 +1,23 @@
-#THINGS TO FIX:
-    #CURRENTLY, BASELINE != 1 DOES NOT WORK
-    #DOES NOT WORK WITH THE as.data.frame(hairEyeColor) DATA SET
+if(!("deamer" %in% installed.packages()[,"Package"])) install.packages("deamer")
+library(deamer)
 
 #' Generate synthetic data
 #'
 #' @param data the data.frame containing the original data
-#' @param baseline the column index for the baseline attribute. Defaults to 1. See details.
+#' @param independent the column index for the independent attribute. Defaults to 1. See details.
 #' @details 
 #'  To generate synthetic data and ensure an appearance of realism, we must
 #'  ensure that relationships between major attributes stay consistent.
-#'  For example, if our data set contained employee information with age and 
-#'  job title, it would be strange if our synthetic data had several 
-#'  Senior Product Managers in their early twenties.
+#'  For example, if our data set contains employee information with age, 
+#'  job title, and location, it would be strange if our synthetic data had several 
+#'  Senior Product Managers in their early twenties living in college towns.
 #'  
-#'  This is where the "baseline attribute" comes in. The baseline attribute
-#'  determines the distribution of the other attributes. When generating a
-#'  synthetic data point, the value of the baseline attribute is chosen
-#'  randomly based on the distribution in the original data.
-#'  Then, to generate every other attribute, we choose the value randomly from
-#'  its distribution, given the baseline attribute.
+#'  This is where the "independent attribute" comes in. The independent attribute
+#'  determines the distribution of the other "dependent" attributes.
+#'  For each entity in the synthetic data set, the value of the independent attribute 
+#'  is generated randomly based on the distribution in the original data.
+#'  Then, to generate the dependent attributes, we choose the value randomly from
+#'  its distribution, given the independent attribute.
 #'  For example, suppose our data set contained the attributes "Job Title", 
 #'  "Age", and "Location". We randomly select the "Senior Programmer" job title.
 #'  In our data set, Senior Programmers mostly are above 40 years old and live in
@@ -26,26 +25,22 @@
 #'  this distribution.
 #'  
 #'  Note that all attributes are treated as categorical.
-#' @return a vector of synthetic data
-
-if(!("deamer" %in% installed.packages()[,"Package"])) install.packages("deamer")
-library(deamer)
-
-generateSynthetic <- function(data, baseline = 1){
+#' @return a data.frame of synthetic data
+generateSynthetic <- function(data, independent = 1){
     dataPrivacyConstant <- 2/0.9    #may be changed at a later release if necessary
     
     #create a distribution of elements for the first column
-    syntheticData <- data.frame(genIndependentAttribute(data, baseline, dataPrivacyConstant))
+    syntheticData <- data.frame(genIndependentAttribute(data, independent, dataPrivacyConstant))
     
     #we grow the synthetic data column-by-column over the non-base attributes
-    nonBaseAttributes <- setdiff(1:ncol(data), baseline)
+    nonBaseAttributes <- setdiff(1:ncol(data), independent)
     for(i in nonBaseAttributes){
         print(i)
-        syntheticData[ , i] <- genAttribute(data, baseline, i, syntheticData[1], dataPrivacyConstant)
+        syntheticData[ , i] <- genAttribute(data, independent, i, syntheticData[1], dataPrivacyConstant)
     }
     
-    if(baseline!=1){
-        originalColumnOrder <- c(2:baseline, 1, (baseline+1):ncol(data))    #insert the baseline column back in its original position
+    if(independent!=1){
+        originalColumnOrder <- c(2:independent, 1, (independent+1):ncol(data))    #insert the independent column back in its original position
         syntheticData <- syntheticData[, originalColumnOrder]
     }
     
@@ -54,6 +49,13 @@ generateSynthetic <- function(data, baseline = 1){
     syntheticData
 }
 
+#' Generate synthetic data for an independent attribute
+#' @param data the data.frame containing the original data
+#' @param independent the column index for the independent attribute in data
+#' @param dataPrivacyConstant the scale of the laplace distribution. A higher value
+#' means a synthetic data distribution farther from the true data distribution.
+#' Defaults to 20/9.
+#' @details 
 #' We assume that our data is split into different subsets.
 #' These subsets are determined by the value of an "independent attribute".
 #' Each subset has its own distribution of attributes.
@@ -62,15 +64,17 @@ generateSynthetic <- function(data, baseline = 1){
 #' Occupation is our category variable
 #' So the distribution of age and the distribution of race depend on what Occupation a person is in
 #' We assume age and race are independent to each other
-genIndependentAttribute <- function(data, baseline, dataPrivacyConstant){
-    baselineTable <- table(data[baseline])  #table with number of occurances of each value of the baseline variable
+#' @return 
+#' a vector of synthetic data for the independent attribute
+genIndependentAttribute <- function(data, independent, dataPrivacyConstant){
+    independentTable <- table(data[independent])  #table with number of occurances of each value of the independent variable
     
-    groups <- names(baselineTable)  #the values of the baseline variable
-    counts <- baselineTable      #number of occurances of each value
+    groups <- names(independentTable)  #the values of the independent variable
+    counts <- independentTable      #number of occurances of each value
     
     #add laplace noise to the counts
     dPcounts <- sapply(counts, function(x){
-        x <- x + rlaplace(1, b = dataPrivacyConstant)
+        x <- x + rlaplace(1, scale = dataPrivacyConstant)
         if(x<0) x=0
         x
     })
@@ -92,28 +96,37 @@ genIndependentAttribute <- function(data, baseline, dataPrivacyConstant){
     syntheticData
 }
 
-#Each new column is generated based on the categories 
-genAttribute <- function(data, baseline, attrIndex, baselineVals, dataPrivacyConstant){
+#' Generate synthetic data for a dependent attribute
+#' @param data the data.frame containing the original data
+#' @param independent the column index for the independent attribute in data
+#' @param dependent the column index for the dependent attribute in data
+#' @param independentVals the synthetic data for the independent atribute
+#' @param dataPrivacyConstant the scale of the laplace distribution. A higher value
+#' means a synthetic data distribution farther from the true data distribution.
+#' Defaults to 20/9.
+#' @return 
+#' a vector of synthetic data for the dependent attribute
+genDependentAttribute <- function(data, independent, dependent, independentVals, dataPrivacyConstant){
     
     #generation works on the assumption that data is categorical
-    data[attrIndex] <- as.factor(unlist(data[attrIndex]))
+    data[dependent] <- as.factor(unlist(data[dependent]))
     
-    #each of the unique baseline values has its own distribution
-    distributions <- unique(unlist(data[baseline]))
+    #each of the unique independent values has its own distribution
+    distributions <- unique(unlist(data[independent]))
     
-    #iterate through each of the distributions (unique values of baseline)
+    #iterate through each of the distributions (unique values of independent)
         #at each one, return a vector of values for the attribute
-    valAtEachDistribution <- sapply(distributions, function(baselineVal){
-        #sameBaselineRows <- data[baseline] == baselineVal
-        sameBaselineRows <- unlist(data[baseline]) == baselineVal
-        data[sameBaselineRows, attrIndex]
+    valAtEachDistribution <- sapply(distributions, function(independentVal){
+        #sameindependentRows <- data[independent] == independentVal
+        sameindependentRows <- unlist(data[independent]) == independentVal
+        data[sameindependentRows, dependent]
     })
     
     names(valAtEachDistribution) <- distributions
     
     #create a matrix from valAtEachDistribution
         #Rows: number of occurances for each value of the new attribute
-        #Cols: each distribution / each unique value of the baseline attribute
+        #Cols: each distribution / each unique value of the independent attribute
     counts <- sapply(valAtEachDistribution, table)
     attrVals <- rownames(counts)
     
@@ -122,7 +135,7 @@ genAttribute <- function(data, baseline, attrIndex, baselineVals, dataPrivacyCon
         oldx <- x
         newx <- x
         if(x!=0) {                          #we only want to add noise to the values that actually occur
-            newx <- x + rlaplace(1, b = dataPrivacyConstant)
+            newx <- x + rlaplace(1, scale = dataPrivacyConstant)
             if(newx<=0) newx <- x/1000      #we don't want to "lose" a value
             #If it falls <=0, then set the probability to a very small value but keep it
             #if x is allowed to become 0, we may have every value have a 0% chance of occuring - and that's no good!
@@ -135,13 +148,13 @@ genAttribute <- function(data, baseline, attrIndex, baselineVals, dataPrivacyCon
     cumProbDist <- apply(probDist, 2, cumsum)
     
     #generate synthetic data from the cumulative probability distributions
-    newColumn <- sapply(unlist(baselineVals), function(baselineVal){    #iterate over the values for the baseline column
-        #print(baselineVal)
-        cPDforthisBaseline <- cumProbDist[, toString(baselineVal)]  #the cumulative probability distribution for this baseline value
+    newColumn <- sapply(unlist(independentVals), function(independentVal){    #iterate over the values for the independent column
+        #print(independentVal)
+        cPDforthisindependent <- cumProbDist[, toString(independentVal)]  #the cumulative probability distribution for this independent value
         
         #generate random val from the attribute's distribution
         r <- runif(1)                               #r is a random number [0,1)
-        attVal <- attrVals[which(r<cPDforthisBaseline)[1]]  #find first group which r falls under
+        attVal <- attrVals[which(r<cPDforthisindependent)[1]]  #find first group which r falls under
     })
     
     #return the synthetic data
